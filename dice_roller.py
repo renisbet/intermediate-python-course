729749f43037cba10b2d68c3edca83b6286fffc4
renisbet <69325137+renisbet@users.noreply.github.com>
2020-08-07 12:10:02 +0100

    Added player 1 variable

--- dice_roller.py
+++ dice_roller.py
@@ -1,7 +1,8 @@
 import random
 
 def main():
-    dice_rolls = int(input('How many dice would you like to roll? '))
+    player_1 = input('Hello! Player 1: what is your name?')
+    dice_rolls = int(input('Hi! How many dice would you like to roll? '))
     dice_size = int(input('How many sides are the dice? '))
     dice_sum = 0
     for i in range(0,dice_rolls):
@@ -13,7 +14,7 @@ def main():
             print(f'You rolled a {roll}! Critical Success!')
         else:
             print(f'You rolled a {roll}')
-    print(f'You have rolled a total of {dice_sum}')
+    print(f'{player_1}, you have rolled a total of {dice_sum}')
 
 if __name__== "__main__":
   main()
